fbace62d7d45d5cd6965bcf0a4e6c25b24ba9218
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2023-08-11 10:33:57 +0200

    Initial state.

---
+++ bookstore/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=1023)
+    description = models.TextField()
+    pages = models.IntegerField()
+    isbn = models.IntegerField(null=True)
+    authors = models.ManyToManyField("Author")
